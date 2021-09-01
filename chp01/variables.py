922adb29d67c53eed52629f8c075aaaf11b14685
jcanaviri <jcanaviri20@gmail.com>
2021-08-31 23:09:05 -0400

    Variables in python

---
+++ chp01/variables.py
@@ -0,0 +1,15 @@
+my_variable = "Hello, from python"
+
+print(my_variable)
+
+my_variable = 10
+print(my_variable)
+
+x = 5
+y = 2
+z = x + y
+
+print(z)
+
+print(type(x))  # Type int
+print(id(x))    # Memory address
