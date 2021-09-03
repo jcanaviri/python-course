eee08ebea7e05f53759d26c2700868369b15dd1a
jcanaviri <jcanaviri20@gmail.com>
2021-09-03 00:36:27 -0400

    Class methods

--- Person.py
+++ Person.py
@@ -4,8 +4,16 @@ class Person:
         self.last_name = last_name
         self.age = age
 
+    def show_detail(self):
+        print(f'Person {self.name} {self.last_name}')
+
 me = Person('Josue', 'Canaviri', 23)
 you = Person('John', 'Doe', 36)
 
 print(f'The object {me.name} {me.last_name} - {me.age}')
 print(f'The object {you.name} {you.last_name} - {you.age}')
+
+me.show_detail()
+you.show_detail()
+
+Person.show_detail(me)
