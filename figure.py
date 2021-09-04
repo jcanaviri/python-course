20104744cbd734cd4d4f62537c79633776eb7f82
jcanaviri <jcanaviri20@gmail.com>
2021-09-03 22:48:52 -0400

    Rectangle class

--- figure.py
+++ figure.py
@@ -1,13 +1,43 @@
 class FiguraGeometrica:
     def __init__(self, ancho, alto):
-        self.ancho = ancho
-        self.alto = alto
+        self._ancho = ancho
+        self._alto = alto
+
+    @property
+    def ancho(self):
+        return self._ancho
+    
+    @ancho.setter
+    def ancho(self, ancho):
+        self._ancho = ancho
+
+    @property
+    def alto(self):
+        return self._alto
+    
+    @alto.setter
+    def alto(self, alto):
+        self._alto = alto
+
+    def __str__(self):
+        return f'<FiguraGeometrica ({self._ancho}, {self._alto})>'
 
 class Color:
     def __init__(self, color):
-        self.color = color
+        self._color = color
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
 
-class Cuadrado(FiguraGeometrica, Color):
+    def __str__(self):
+        return f'<Color {self._color}>'
+
+class Cuadrado(Color, FiguraGeometrica):
     def __init__(self, lado, color):
         FiguraGeometrica.__init__(self, lado, lado)
         Color.__init__(self, color)
@@ -15,6 +45,26 @@ class Cuadrado(FiguraGeometrica, Color):
     def area(self):
         return self.ancho * self.alto
 
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)} {Color.__str__(self)}'
+
+class Rectangulo(FiguraGeometrica, Color):
+    def __init__(self, ancho, alto, color):
+        FiguraGeometrica.__init__(self, ancho, alto)
+        Color.__init__(self, color)
+    
+    def area(self):
+        return self.ancho * self.alto
+
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)} {Color.__str__(self)}'
+
 cuadrado = Cuadrado(5, 'red')
 
-print(cuadrado.area())
+# MRO Method resolution order
+print(Cuadrado.mro())
+
+rectangulo = Rectangulo(4, 5, 'blue')
+
+print(rectangulo)
+print(rectangulo.area())
