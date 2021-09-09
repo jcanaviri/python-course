84a4a00d212d119ae000ea71a347fdb34bac948c
jcanaviri <jcanaviri20@gmail.com>
2021-09-09 08:42:26 -0400

    Multiple exeptions

--- Exceptions/use_exeptions.py
+++ Exceptions/use_exeptions.py
@@ -4,7 +4,12 @@ b = 0
 
 try:
     result = a / b
+except ZeroDivisionError as e:
+    print(f'Zero division: An error happend: {e}')
 except TypeError as e:
-    print(f'An error happend: {e}')
+    print(f'Type error: An error happend: {e}')
+except Exception as e:
+    print(f'Exception: An error happend: {e}')
+
 print(f'Result: {result}')
 print('Continuamos...')
