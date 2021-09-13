d33f30d27840b04715d3ba1ac99fe49074189a79
jcanaviri <jcanaviri20@gmail.com>
2021-09-13 15:34:16 -0400

    Using fetchone

---
+++ databases/get_one.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+# Credentials for database connection
+conn = psycopg2.connect(
+    user='postgres',
+    password='Josue123$',
+    host='localhost',
+    port='5432',
+    database='test_db'
+)
+
+try:
+    with conn:
+        with conn.cursor() as cursor:
+            sql = 'SELECT * FROM public.person WHERE person_id=%s;'
+            person_id = input('Write the person_id that you want: ')
+            cursor.execute(sql, (person_id,))
+            result = cursor.fetchone()
+            print(result)
+except Exception as e:
+    print(f'Some error happend: {e}')
+finally:
+    conn.close()
