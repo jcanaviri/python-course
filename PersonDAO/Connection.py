463e942d60f629291fe5137180e83178fd6b0e81
jcanaviri <jcanaviri20@gmail.com>
2021-09-21 09:59:03 -0400

    Set up connection

--- PersonDAO/Connection.py
+++ PersonDAO/Connection.py
@@ -1,4 +1,4 @@
-import psycopg2 as db, pool
+import psycopg2 as db
 import sys
 
 from logger_file import log
@@ -9,30 +9,8 @@ class Connection:
     _PASSWORD = 'Josue123$'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _MIN_CONN = 1
-    _MAX_CONN = 5
-    _pool = None
-
-    @classmethod
-    def get_pool(cls):
-        if cls._pool is None:
-            try:
-                cls._pool = pool.SimpleConnectionPool(
-                    cls._MIN_CONN, 
-                    cls._MAX_CONN,
-                    host=cls._HOST,
-                    user=cls._USERNAME,
-                    password=cls._PASSWORD,
-                    port=cls._DB_PORT,
-                    database=cls._DATABASE)
-                
-                log.debug(f'Pool created Successfully {cls._pool}')
-                return cls._pool
-            except Exception as e:
-                log.error(f'An error in pool happend')
-                sys.exit()
-        else:
-            return cls._pool
+    _conn = None
+    _cursor = None
 
     @classmethod
     def get_connection(cls):
