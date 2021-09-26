79850ddfcb34ffc899b868654ad30beb3161c307
jcanaviri <jcanaviri20@gmail.com>
2021-09-26 15:28:38 -0400

    Usando unicode

---
+++ Numbers/unicode.py
@@ -0,0 +1,12 @@
+print('Hello\u0020World')
+
+print('Notacion simple: \u0041')
+print('Notacion extendida: \U00000041')
+print('Notacion hexagecimal: \x41')
+
+print('Heart \u2665')
+print('Smile \U0001f600')
+print('Snake \U0001f40d')
+
+character = chr(64)
+print(character)
