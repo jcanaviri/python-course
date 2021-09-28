1c5bd11783271ef52cb4a564b61904535dac4ddf
jcanaviri <jcanaviri20@gmail.com>
2021-09-27 23:58:59 -0400

    Basic of sets

---
+++ Sets/sets.py
@@ -0,0 +1,23 @@
+# Sets
+my_set = {1, 2, 3, 4}
+
+print(my_set)
+print(type(my_set))
+
+# Wrong way
+empty_set = {}
+print(type(empty_set))
+
+# Correct way
+emtpy_set = set()
+print(emtpy_set)
+print(type(emtpy_set))
+
+my_set.add('Me')
+my_set.add('You')
+my_set.add('Me')
+print(my_set)
+
+my_set = set([6, 7, 8])
+print(my_set)
+
