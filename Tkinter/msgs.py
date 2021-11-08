d8be9bea26bc2da5b49c746a820297b059c1e885
jcanaviri <jcanaviri20@gmail.com>
2021-11-08 15:59:00 -0400

    Add a menu with options

--- Tkinter/msgs.py
+++ Tkinter/msgs.py
@@ -1,6 +1,7 @@
+import sys
 import tkinter as tk
 
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, Menu
 
 window = tk.Tk()
 window.geometry('600x400')
@@ -17,11 +18,30 @@ def send():
 
     if msg:
         messagebox.showinfo('Message title', f'Message content { msg }')
-        messagebox.showerror('Message title', f'Message content { msg }')
-        messagebox.showwarning('Message title', f'Message content { msg }')
 
+def close_window():
+    window.quit()
+    window.destroy()
+    sys.exit()
+
+def create_menu():
+    """Settings for the main menu"""
+    main_menu = Menu()
+    sub_menu = Menu(main_menu, tearoff=False)
+    sub_menu.add_command(label='New')
+    sub_menu.add_separator()
+    sub_menu.add_command(label='Close', command=close_window)
+    main_menu.add_cascade(menu=sub_menu, label='File')
+
+    help_menu = Menu(main_menu, tearoff=False)
+    help_menu.add_command(label='About us')
+    main_menu.add_cascade(menu=help_menu, label='Help')
+
+    window.config(menu=main_menu)
 
 btn1 = ttk.Button(window, text='send', command=send)
 btn1.grid(row=0, column=1)
+
+create_menu()
 
 window.mainloop()
